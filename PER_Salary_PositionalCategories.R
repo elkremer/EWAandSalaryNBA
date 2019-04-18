cbf4ae945a961c3db7d80961d159cfbdc8bee3de
Peter Kremer <elkremer@yahoo.com>
2019-04-18 18:04:59 -0400

    Latest version.

--- PER_Salary_PositionalCategories.R
+++ PER_Salary_PositionalCategories.R
@@ -5,6 +5,9 @@ library(kernlab)
 library(e1071)
 library(caret)
 
+#Turn off scientific notation
+options(scipen=999)
+
 #Filters data for the stats<year> data set
 filterStatData <- function (stats) {
   #Keep only players who played at least 500 minutes during the season
@@ -62,7 +65,7 @@ stats2017 <- filterStatData(stats2017)
 advStats2017 <- filterStatData(advStats2017)
 stats2018 <- filterStatData(stats2018)
 
-
+#Make salary just numbers
 sal2017$Salary <- as.numeric(gsub('[$,]', '', sal2017$Salary))
 sal2018$Salary <- as.numeric(gsub('[$,]', '', sal2018$Salary))
 
@@ -73,15 +76,16 @@ Playoffs2018$Playoffs <- as.factor(Playoffs2018$Playoffs)
 cd <- advStats2017[,c(9,10,11,12,13,14,15,16,17,18,19,20,21)]
 
 #Some EDA for salary and EWA
-boxplot(sal2017$Salary, main = "Salary for 2017", ylab="Salary in US$")
-boxplot(sal2018$Salary, main = "Salary for 2018", ylab="Salary in US$")
+boxplot(sal2017$Salary / 1000000, main = "Salary for 2017 in Millions US$", ylab="Salary in Millions US$")
+boxplot(sal2018$Salary / 1000000, main = "Salary for 2018 in Millions US$", ylab="Salary in Millions US$")
 boxplot(EWA2017$EWA, main = "EWA for 2017", ylab="Estimated Wins Added (EWA)")
 boxplot(EWA2018$EWA, main = "EWA for 2018", ylab="Estimated Wins Added (EWA)")
-hist(sal2017$Salary)
-hist(sal2018$Salary)
-hist(EWA2017$EWA)
-hist(EWA2018$EWA)
+hist(sal2017$Salary / 1000000, main = "Salary for 2017 in US$ Millions", xlab = "Salary in Millions US Dollars")
+hist(sal2018$Salary / 1000000, main = "Salary for 2018 in US$ Millions", xlab = "Salary in Millions US Dollars")
+hist(EWA2017$EWA, main = "Estimated Wins Added (EWA) for 2017", xlab = "Estimates Wins Added (EWA)")
+hist(EWA2018$EWA, main = "Estimated Wins Added (EWA) for 2018", xlab = "Estimates Wins Added (EWA)")
 
+#Different groups of stats for k-means
 clusdata <- stats2017[,c(12,13,15,17,21,22,24,25,26,27,28,29)]
 clusdata2 <- stats2017[,c(12,17,19,20,21,22,24,25,26,27,29)]
 clusdata3 <- stats2017[,c(11,12,18,23,24,25,26,27,29)]
@@ -105,16 +109,20 @@ for (k in krange) {
   totw.ss[k-4] <- mean(tmp.totw.ss) #average of the withinss
 }
 
+#create scree plot to identify elbow for best value of k
 plot(krange, totw.ss, type = "b", main="Total Within SS by Various K",
      ylab="Average Total Within Sum of Squares",
      xlab="Value of K")
 
 #tmpCls1 <- kmeans(stats2017[,c(4,10,11,12,13,14,16,17,18,19,20,21,22,23,25,26,27,28,29,30)], centers = 7, nstart = 20)
 tmpCls1 <- kmeans(stats2017[,c(4,12,13,17,21,22,24,25,26,27,28,29)], centers = 7, nstart = 20)
+#Create a new column for the cluster
 stats2017$newPos <- tmpCls1$cluster
 t <- stats2017[,c(4,12,13,17,21,22,24,25,26,27,28,29)]
+
 #t <- stats2017[,c(4,10,11,12,13,14,16,17,18,19,20,21,22,23,25,26,27,28,29,30)]
 tmpClsPlot1 <- clusplot(t, tmpCls1$cluster, color = TRUE, main = "K-Means k = 7 with traditional statistics")
+#Table of traditional position vs cluster
 table(stats2017$Pos, stats2017$newPos)
 
 tmpCls <- kmeans(stats2017[,c(4,31,32)], centers = 8, nstart = 25)
@@ -128,7 +136,6 @@ plot(den)
 
 #Look at the results
 table(stats2017$Pos, stats2017$newPos)
-
 
 ##Merge the data sets together for further EDA and analysis
 #Merge the EWA and salary data for 2017
@@ -145,15 +152,15 @@ bymedian <- with(mrg17, reorder(Tm, EWA, median))
 boxplot(mrg17$EWA ~ bymedian, main = 'EWA by Team 2017', ylab = 'Estimated Wins Added (EWA)', las = 3)
 #boxplot(mrg17$EWA ~ mrg17$Tm, at=rank(tapply(mrg17$EWA , mrg17$Tm, median)))
 byMedSal2017 <- with(mrg17, reorder(Tm, Salary, median))
-boxplot(mrg17$Salary ~ byMedSal2017, main = 'Salary by Team 2017', ylab = 'Salary in US$', las = 3)
+boxplot(mrg17$Salary / 1000000 ~ byMedSal2017, main = 'Salary by Team 2017 in Millions US$', ylab = 'Salary in Millions US$', las = 3)
 
 
 #Variable for ordering the medians for the boxplot
 byMedEWA2018 <- with(mrg18, reorder(Tm, EWA, median))
 boxplot(mrg18$EWA ~ byMedEWA2018, main = 'EWA by Team 2018', ylab = 'Estimated Wins Added (EWA)', las = 3)
-boxplot(mrg18$Salary ~ mrg18$Tm)
+#boxplot(mrg18$Salary / 1000000 ~ mrg18$Tm)
 byMedSal2018 <- with(mrg18, reorder(Tm, Salary, median))
-boxplot(mrg18$Salary ~ byMedSal2018, main = 'Salary by Team 2018', ylab = 'Salary in US$', las = 3)
+boxplot(mrg18$Salary / 1000000 ~ byMedSal2018, main = 'Salary by Team 2018 in Millions US$', ylab = 'Salary in Millions US$', las = 3)
 
 ##Look at position
 #EWA by postion for 2017
@@ -172,21 +179,23 @@ boxplot(mrg18$Salary ~ mrg18$Position)
 mrg17$EWA[mrg17$EWA == 0] <- .1
 mrg18$EWA[mrg18$EWA == 0] <- .1
 
-mrg17$wincost <- mrg17$Salary / mrg17$EWA 
-boxplot(mrg17$wincost ~ mrg17$Tm, las = 3, main = "wincost by team 2017")
-mrg18$wincost <- mrg18$Salary / mrg18$EWA 
-boxplot(mrg18$wincost ~ mrg18$Tm, las = 3, main = "wincost by team 2018")
+mrg17$wincost <- mrg17$Salary / mrg17$EWA
+byMedWincost2017 <- with(mrg17, reorder(Tm, wincost, median))
+boxplot(mrg17$wincost / 1000000 ~ byMedWincost2017, las = 3, main = "wincost by team 2017 in Millions US$", ylab = "wincost in Millions US$")
+mrg18$wincost <- mrg18$Salary / mrg18$EWA
+byMedWincost2018 <- with(mrg18, reorder(Tm, wincost, median))
+boxplot(mrg18$wincost / 1000000 ~ byMedWincost2018, las = 3, main = "wincost by team 2018  in Millions US$", ylab = "wincost in Millions US$")
 
 #############Good to here
 tm17 <- select(mrg17, wincost, Salary, Player, Tm, PosNum.x, Position, PER, VA, EWA)
 tm18 <- select(mrg18, wincost, Salary, Player, Tm, PosNum.x, Position, PER, VA, EWA)
 
 ggplot(data = tm17) +
-  geom_point(mapping = aes(x = EWA, y = Salary, color = Position)) +
-  labs(title = "EWA vs. Salary for 2017")
+  geom_point(mapping = aes(x = EWA, y = Salary / 1000000, color = Position)) +
+  labs(title = "EWA vs. Salary for 2017", y = "Salary in Millions US$")
 ggplot(data = tm18) +
-  geom_point(mapping = aes(x = EWA, y = Salary, color = Position)) +
-  labs(title = "EWA vs. Salary for 2018")
+  geom_point(mapping = aes(x = EWA, y = Salary / 1000000, color = Position)) +
+  labs(title = "EWA vs. Salary for 2018",  y = "Salary in Millions US$")
 #Create a boxplot of the wincost
 outwincost <- boxplot(tm17$wincost)
 #Identify outliers
@@ -212,20 +221,28 @@ train <- merge(train, Playoffs2017, by.x = "Tm", by.y = "TeamName")
 test <- merge(test, Playoffs2018, by.x = "Tm", by.y = "TeamName")
 
 ggplot(data = train) +
-  geom_point(mapping = aes(x = EWATot, y = SalTot, color = Playoffs))
+  geom_point(mapping = aes(x = EWATot, y = SalTot / 1000000, color = Playoffs)) +
+  labs(title = "Team EWA vs. Team Salary for 2016-17 Season", y = "Team Salary in Millions US$", x = "Cummulative Team EWA")
 ggplot(data = test) +
-  geom_point(mapping = aes(x = EWATot, y = SalTot, color = Playoffs))
-#SVM time
+  geom_point(mapping = aes(x = EWATot, y = SalTot / 1000000, color = Playoffs)) +
+  labs(title = "Team EWA vs. Team Salary for 2017-18 Season", y = "Team Salary in Millions US$", x = "Cummulative Team EWA")
 
+#SVM time
 #model <- svm(Playoffs ~ EWATot + SalTot, data = train, type = "C-classification", kernel = 'polynomial')
 #preds <- predict(model, test)
-model <- svm(train$EWATot + train$SalTot, train$Playoffs, type = "C-classification", kernel = 'polynomial')
+model <- svm(train$EWATot + train$SalTot, train$Playoffs, cost = .1, gamma = 1, type = "C-classification", kernel = 'polynomial')
 preds <- predict(model, test$EWATot + test$SalTot)
 table(preds)
 test$preds <- preds
 table(test$preds, test$Playoffs)
 results <- confusionMatrix(data = test$preds, reference = test$Playoffs, positive = '1')
 print(results)
+
+ggplot(data = test) +
+  geom_point(mapping = aes(x = SalTot / 1000000, y = EWATot, color = Playoffs, shape = preds)) +
+  labs(title = "Team EWA vs. Team Salary with Playoffs and Predictions", x = "Team Salary in Millions US$", y = "Cummulative Team EWA")
+
+obj <- tune.svm(train$EWATot + train$SalTot, train$Playoffs, type = "C-classification", kernel = 'sigmoid', cost = c(.1,.2,.4,1,2))
 #fit <- ksvm(EWATot~SalTot,data = train,kernel = "tanhdot", type = "C-svc")
 #fit <- ksvm(Playoffs ~ .,data = train,kernel = "rbfdot", type = "C-svc")
 #SV <- train[alphaindex(fit)[[1]],]
